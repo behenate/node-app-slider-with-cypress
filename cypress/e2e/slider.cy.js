4651f5a04e69ac6eb0ead918e58d34084e7e53d1
Wojciech Dróżdż <behenate@gmail.com>
2024-03-19 17:23:11 +0100

    test

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -1,6 +1,6 @@
 describe('Swiper Gallery Test', function () {
   it('Checks if second slide contains "United Kingdom"', function () {
-    cy.visit('http://app:3000');
+    cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
   });
@@ -8,11 +8,24 @@ describe('Swiper Gallery Test', function () {
 
 describe('Swiper Gallery Test', function () {
   it('Checks if third slide contains "Paris"', function () {
-    cy.visit('http://app:3000');
+    cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.wait(2000)
     cy.get('.swiper-button-next').click({ force: true });
     cy.wait(2000)
     cy.get('.swiper-slide-active').should('contain', 'Paris');
+  });
+});
+// Zweryfikuj, czy opis każdego slajdu jest wyświetlany poprawnie.
+describe('Swiper Gallery Test', function () {
+  it('Chceck if every slide contains a description"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.card-description').should('not.be.empty').should('contain', 'Italy')
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000)
+    cy.get('.card-description').should('not.be.empty').should('contain', 'United Kingdom')
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000)
+    cy.get('.card-description').should('not.be.empty').should('contain', 'France')
   });
 });
